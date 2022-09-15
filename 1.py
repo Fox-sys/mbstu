30102119cf3e01d0c3a8d689d051bc4350b31b04
fox-sys <borisberestov@ya.ru>
2022-09-15 12:42:17 +0300

    init

---
+++ 1.py
@@ -0,0 +1,22 @@
+# a = 20.
+# print(type(a))
+#
+#
+# b = True
+# print('\n', type(b))
+
+a = 21
+b = 3
+
+# print(c := a + b, f" - {type(c)}")
+# try:
+#     print(c := a - b, f" - {type(c)}")
+#     print(c := a * b, f" - {type(c)}")
+#     print(c := a / b, f" - {type(c)}")
+#     print(c := a ** b, f" - {type(c)}")
+#     print(c := a % b, f" - {type(c)}")
+#     print(c := a // b, f" - {type(c)}")
+# except TypeError:
+#     pass
+
+print(bin(a), bin(b))
